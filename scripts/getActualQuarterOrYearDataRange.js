1a25fa424935cd9dd84d89d1f63f692637bcbd52
techopse2e <38204413+techopse2e@users.noreply.github.com>
2023-08-10 11:18:18 +0800

    Create getActualQuarterOrYearDataRange.js

---
+++ scripts/getActualQuarterOrYearDataRange.js
@@ -0,0 +1,4 @@
+// This script is using to get actual quarter or year data range, given a day and level
+// example:
+// given 2023-04-01 and level is quarter, will return 2023-04-01 - 2023-06-30
+// given 2022-10-01 and level is year, will return 2022-01-01 - 2022-12-31
